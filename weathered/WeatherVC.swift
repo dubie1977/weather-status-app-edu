a2d8ba06294e590255189fb70294676875f9a04b
Joseph DuBay <dubie1977@Josephs-iMac.local>
2017-07-30 23:16:22 -0700

    more work on ui

--- weathered/WeatherVC.swift
+++ weathered/WeatherVC.swift
@@ -10,10 +10,24 @@ import Cocoa
 
 class WeatherVC: NSViewController {
 
+    //outlets
+    @IBOutlet weak var dateLbl: NSTextField!
+    @IBOutlet weak var tempLbl: NSTextField!
+    @IBOutlet weak var locationLbl: NSTextField!
+    @IBOutlet weak var weatherImage: NSImageView!
+    @IBOutlet weak var weatherConditionLbl: NSTextField!
+    @IBOutlet weak var collectionView: NSCollectionView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear() {
+        self.view.layer?.backgroundColor = CGColor(red: 0.29, green: 0.72, blue: 0.98, alpha: 1.0)
     }
 
     override var representedObject: Any? {
